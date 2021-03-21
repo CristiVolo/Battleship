2a84adf444f87023b057074a54851c6a7a498439
hciulica <62338899+hciulica@users.noreply.github.com>
2021-03-21 22:43:00 +0200

    Initialize springboot for backend

---
+++ Backend/spring-boot-battleship/src/test/java/com/theimpostors/battleship/SpringBootBattleshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theimpostors.battleship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBattleshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
