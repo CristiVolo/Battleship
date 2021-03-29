725b35cb45d04c7f0ae9a18da1e290e62866d08b
hciulica <62338899+hciulica@users.noreply.github.com>
2021-03-29 16:42:48 +0300

    Frontend-Home

--- Battleship/src/app/app.module.ts
+++ Battleship/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomecarriersigninComponent } from './components/homecarriersignin/homecarriersignin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomecarriersigninComponent
   ],
   imports: [
     BrowserModule
---
+++ Battleship/src/app/components/homecarriersignin/homecarriersignin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homecarriersignin',
+  templateUrl: './homecarriersignin.component.html',
+  styleUrls: ['./homecarriersignin.component.css']
+})
+export class HomecarriersigninComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
